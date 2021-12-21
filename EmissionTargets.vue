7b3e71e960d161a470892bf4314585097d5bd96b
kultarsall <72468879+kultarsall@users.noreply.github.com>
2021-12-21 22:36:13 +0000

    Add files via upload

---
+++ EmissionTargets.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-row justify="center" class="mt-5">
+        <v-col lg="6" md="12">
+            <h2>What do you mean by 'Carbon Footprint'?</h2>
+            <p>
+                Your footprint is a way of showing your carbon emissions, compared to other people and other countries. It's your impression on the planet.
+            </p>
+            <p>
+                By carbon emissions, we mean greenhouse gases, including carbon dioxide, methane and nitrous oxide. Humans produce these gases in vast quantities by doing things like burning coal, oil and gas for energy and cutting down forests.
+                Your individual emissions are built up from the energy you use personally for electricity and travel, as well as the energy that's required to produce your food and all the other stuff you buy, whether it's made in the UK or elsewhere in the world.
+            </p>
+            <p>
+                We convert all the different greenhouse gases into an equivalent impact from carbon dioxide, the main greenhouse gas. Your footprint value is in tonnes of carbon dioxide equivalent
+            </p>
+
+            <v-divider class="mb-5"></v-divider>
+
+            <h2>2020 & 2050 Target</h2>
+            <p>
+                The Climate Change Act 2008 established a target for the UK to reduce its emissions by at least 80% from 1990 levels by 2050.
+                In 2019 the target was enhanced to at least 100% (net-zero) by 2050. To help us get there, there are targets along the way, and we need to be 37% of the way there by 2020.
+            </p>
+            <p>
+                You can find out more here:
+                <a href="https://www.theccc.org.uk/tackling-climate-change/reducing-carbon-emissions/carbon-budgets-and-targets/" target="_blank" rel="noopener noreferrer">
+                    https://www.theccc.org.uk/tackling-climate-change/reducing-carbon-emissions/carbon-budgets-and-targets/
+                </a>
+            </p>
+            <p>
+                The 2020 UK target to reach net-zero by 2050 is 10.5 tonnes. Indivuals should strive for a target bellow 10.5 tonnes.
+            </p>
+            <p>
+                The UK average emission as of 2020 is 12.7 tonnes. You would need to drive 23,000 miles in the average car to emit 12.7 tonnes of CO<sub>2</sub>e
+            </p>
+        </v-col>
+    </v-row>
+</template>
